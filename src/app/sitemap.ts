4b071b6f82072ad7bd2d14ba243b9e33ee7f1296
andre <andreamusholin@gmail.com>
2025-09-25 17:03:16 +0700

    menambahkan sitemap.ts

---
+++ src/app/sitemap.ts
@@ -0,0 +1,20 @@
+// app/sitemap.ts
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://op2-bocah-teknik-pengelasan.vercel.app', // Ganti dengan URL utama Anda
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    // Tambahkan URL lain jika ada, misalnya halaman blog atau layanan
+    // {
+    //   url: 'https://nama-domain-anda.com/about',
+    //   lastModified: new Date(),
+    //   changeFrequency: 'monthly',
+    //   priority: 0.8,
+    // },
+  ]
+}
