91e4f4ddbfb46a24b16e77cd949ef5d308246b09
andre <andreamusholin@gmail.com>
2025-09-26 15:30:25 +0700

    edit localBussinesSchema

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,45 @@ import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import ServiceList from "./components/ServiceList";
 
+function LocalBusinessSchema() {
+  const schema = {
+    "@context": "https://schema.org",
+    "@type": "LocalBusiness",
+    "name": "Bocah Teknik",
+    "description": "Jasa las profesional untuk kanopi, pagar, teralis, dan instalasi baru di wilayah Yogyakarta.",
+    "address": {
+      "@type": "PostalAddress",
+      "streetAddress": "Kedongdowo Wetan RT 18/RW 04, Pampang, Paliyan",
+      "addressLocality": "Gunungkidul",
+      "addressRegion": "Daerah Istimewa Yogyakarta",
+      "addressCountry": "ID"
+    },
+    "telephone": "+6289637606973"
+    // Anda juga bisa menambahkan "openingHours", "image", dll.
+  };
+
+  return (
+    <script
+      type="application/ld+json"
+      dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
+    />
+  );
+}
+
 export default function HomePage() {
   return (
-    <main>
-      <Navbar />
-      <Hero />
-      <ServiceList />
-      {/* <Gallery /> */}
-      <ContactInfo />
-      <Footer/>
+    <>
+      <LocalBusinessSchema />
+      <main>
+        <Navbar />
+        <Hero />
+        <ServiceList />
+        {/* <Gallery /> */}
+        <ContactInfo />
+        <Footer />
 
-      {/* Anda bisa menambahkan footer di sini jika perlu */}
-    </main>
+        {/* Anda bisa menambahkan footer di sini jika perlu */}
+      </main>
+    </>
   );
 }
