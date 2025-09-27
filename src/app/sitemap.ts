1542f8c4f92cdeb725fb244f137eec6a274be000
andre <andreamusholin@gmail.com>
2025-09-27 15:50:19 +0700

    membenahi sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,20 +1,24 @@
 // app/sitemap.ts
 import { MetadataRoute } from 'next'
+import servicesData from './dataStatic/servicesData'
+// import { url } from 'inspector'
 
 export default function sitemap(): MetadataRoute.Sitemap {
+  const servicesPageUrls = servicesData.map(service => ({
+    url: `https://bocahteknik.my.id/layanan/${service.slug}`,
+    lastModified: new Date(),
+    changeFrequency: 'monthly' as const,
+    priority: 0.8,
+  }));
+
   return [
     {
-      url: 'https://www.bocahteknik.my.id/', // Ganti dengan URL utama Anda
+      url: 'https://bocahteknik.my.id/', // Ganti dengan URL utama Anda
       lastModified: new Date(),
-      changeFrequency: 'yearly',
+      changeFrequency: 'monthly',
       priority: 1,
     },
-    // Tambahkan URL lain jika ada, misalnya halaman blog atau layanan
-    // {
-    //   url: 'https://nama-domain-anda.com/about',
-    //   lastModified: new Date(),
-    //   changeFrequency: 'monthly',
-    //   priority: 0.8,
-    // },
+    ...servicesPageUrls,
+    
   ]
 }
